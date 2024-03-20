234bd5a38c69017d1dd20ce439a19da0b2986181
admin <vladislav_nadein@mail.ru>
2024-03-20 13:00:21 +0300

    Made a Card object

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,7 +1,7 @@
 package app;
 
 public class App {
-    void main(){
+    public static void main(String[] args){
 
     }
 }
--- src/main/java/bank/Card.java
+++ src/main/java/bank/Card.java
@@ -1,4 +1,47 @@
 package bank;
 
 public class Card {
+    private final long CARD_NUMBER;
+
+
+    private int pinCode;
+
+    public Card(long CARD_NUMBER) {
+        this.CARD_NUMBER = CARD_NUMBER;
+        generatePinCode();
+    }
+
+    private void generatePinCode() {
+        StringBuilder generatingPin = new StringBuilder();
+        for(int i = 0; i<4;i++){
+            generatingPin.append(rnd(0, 9));
+        }
+        pinCode = Integer.parseInt(generatingPin.toString());
+    }
+
+    public long getCARD_NUMBER() {
+        return CARD_NUMBER;
+    }
+
+    public int getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(int pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    public static void main(String[] args){
+        Card firstCard = new Card(1231231231231231L);
+        System.out.println("Номер карты: " + firstCard.getCARD_NUMBER());
+        System.out.println("ПИН: " + firstCard.getPinCode());
+//        System.out.println(Card.CARD_NUMBER);
+//        System.out.println(new Card(pinCode));
+    }
+
+    public static int rnd(int min, int max)
+    {
+        max -= min;
+        return (int) (Math.random() * ++max) + min;
+    }
 }
